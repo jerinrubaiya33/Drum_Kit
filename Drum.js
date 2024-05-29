a4dfffe2678e203d7c3e071656b86c208a39dc51
Rubaiya Khan Jerin <163726222+jerinrubaiya33@users.noreply.github.com>
2024-05-30 00:14:42 +0600

    Add files via upload

---
+++ Drum.js
@@ -0,0 +1,111 @@
+document.getElementById('r').addEventListener('click', function() {
+    var audio = document.getElementById('ru');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+    }
+});
+document.getElementById('u').addEventListener('click', function() {
+    var audio = document.getElementById('ub');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+    }
+});
+
+
+document.getElementById('b').addEventListener('click', function() {
+    var audio = document.getElementById('ba');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+    }
+});
+
+
+document.getElementById('a').addEventListener('click', function() {
+    var audio = document.getElementById('ai');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+    }
+});
+document.getElementById('i').addEventListener('click', function() {
+    var audio = document.getElementById('ia');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+    }
+});
+document.getElementById('y').addEventListener('click', function() {
+    var audio = document.getElementById('ye');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+    }
+});
+document.getElementById('a1').addEventListener('click', function() {
+    var audio = document.getElementById('myAudio');
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Course
+// var drumButtons = document.querySelectorAll(".drum");
+// var numberOfDrumButtons = drumButtons.length;
+
+// for (var i = 0; i < numberOfDrumButtons; i++) {
+//     drumButtons[i].addEventListener("click", function() {
+//         var audio = new Audio("c:\Users\RUBAIY~1\AppData\Local\Temp\Rar$DRa8140.4717.rartemp\Drum Kit Completed\sounds\tom-3.mp3");
+//         audio.play();
+//     });
+// }
+
+
+
+
+// YouTube
+// let play = document.getElementById("drum");
+// function playMusic(){
+//     let audio = new Audio ("c:\Users\RUBAIY~1\AppData\Local\Temp\Rar$DRa8140.4717.rartemp\Drum Kit Completed\sounds\tom-3.mp3");
+//     audio.play()
+// }
+// play.addEventListener("click",playMusic);
+
+
+
+
+
+
+// THIS ONE WORKS 
+// var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+// for (var i = 0; i<numberOfDrumButtons; i++){
+//     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        
+//         alert("I got");
+//     });
+// }
